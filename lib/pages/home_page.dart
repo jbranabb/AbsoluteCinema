b32d81606018f0f0fd4e881866192940b991bcdf
Jibranab <zibranaby069@gmail.com>
2025-05-25 12:15:23 +0700

    feat(home_page) add some navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,11 @@
-import 'dart:math';
+import 'dart:ui';
 
 import 'package:absolutecinema/pages/widgets/appbar.dart';
+import 'package:absolutecinema/pages/widgets/slider.dart';
 import 'package:absolutecinema/state/bloc/home_bloc.dart';
-import 'package:absolutecinema/state/cubit/dot_indicator.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:carousel_indicator/carousel_indicator.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,8 +16,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   CarouselSliderController scrollController = CarouselSliderController();
-  int inerCurrentPage = 0;
-  bool b = false;
   @override
   void initState() {
     super.initState();
@@ -30,172 +25,71 @@ class _HomePageState extends State<HomePage> {
   int lenght = 0;
   @override
   Widget build(BuildContext context) {
-    print('check inner page $inerCurrentPage');
     var mediaQuery = MediaQuery.of(context).size;
     return Scaffold(
-      body: BlocBuilder<HomeBloc, HomeState>(
-        builder: (context, state) {
-          if (state is StateLoaded) {
-            return Stack(
-              children: [
-                Container(
-                  height: double.infinity,
-                  width: double.maxFinite,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                        Colors.blue.shade900.withOpacity(0.5),
-                        Colors.transparent,
-                      ])),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 10),
-                    MyAppBar(),
-                    Slider(scrollController: scrollController),
-                  ],
-                ),
-              ],
-            );
-          } else if (state is StateLoading) {
-            return Column(
-              children: [
-                Container(
-                  height: 30,
-                  width: 200,
-                  color: Colors.blue.shade900,
-                )
-              ],
-            );
-          } else if (state is StateError) {
-            return Container(
-              child: Center(
-                child: Text(
-                  'Something went Wrong ${state.e}',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                  Colors.blue.shade900.withOpacity(0.5),
+                  Colors.blue.shade900.withOpacity(0.2),
+                  Colors.transparent,
+                ],
+                    stops: [
+                  0.0,
+                  0.3,
+                  0.7
+                ])),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 1.0, sigmaY: 1.0),
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              MyAppBar(),
+              SliderWidget(),
+              // Container(
+              //   color: Colors.red,
+              //   height: 300,
+              // )
+            ],
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        height: 40,
+        width: 200,
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius:BorderRadius.circular(15) ,
+              child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+              child: Container(color: Colors.white.withOpacity(0.2),),
               ),
-            );
-          }
-          return Container(child: const Text('YOu got me'));
-        },
+            ),
+            Row(
+              children: [
+                IconButton(onPressed: (){}, icon: Icon(Icons.home)),
+                IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+                IconButton(onPressed: (){}, icon: Icon(Icons.save)),
+                IconButton(onPressed: (){}, icon: Icon(Icons.person)),
+              ],
+            )
+          ],
+        ),
       ),
     );
-  }
-}
-
-class Slider extends StatelessWidget {
-  const Slider({
-    super.key,
-    required this.scrollController,
-  });
-
-  final CarouselSliderController scrollController;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<HomeBloc, HomeState>(builder: (context, state) {
-      if (state is StateLoaded) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // text dicover
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                'Discover Youre Next\nFavorite Movie.',
-                textAlign: TextAlign.start,
-                style: GoogleFonts.interTight(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700,
-                  fontSize: 23,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-                height: 180,
-                width: double.infinity,
-                child: CarouselSlider.builder(
-                    carouselController: scrollController,
-                    itemCount: min(10, state.trending.length),
-                    itemBuilder: (context, index, realIndex) {
-                      var movies = state.trending[index];
-                      return Stack(
-                        children: [
-                          Container(
-                            width: 580,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: CachedNetworkImage(
-                                fit: BoxFit.cover,
-                                imageUrl:
-                                    'https://image.tmdb.org/t/p/w300${movies.posterPath}',
-                                errorWidget: (context, url, error) => Center(
-                                  child: Text('Something Went Wrong $error'),
-                                ),
-                                placeholder: (context, url) => Center(
-                                  child: Text(movies.title),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      );
-                    },
-                    options: CarouselOptions(
-                      autoPlay: true,
-                      enlargeCenterPage: true,
-                      viewportFraction: 0.8,
-                      onPageChanged: (index, reason) {
-                        context.read<DotIndicator>().changeState(index);
-                      },
-                      autoPlayAnimationDuration: const Duration(seconds: 1),
-                      // autoPlayInterval: Duration(seconds: 60),
-                      // autoPlayCurve: Cur
-                    ))),
-            BlocBuilder<DotIndicator, int>(builder: (context, currentIndex) {
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 110, vertical: 10),
-                child: Row(
-                    children: List<Widget>.generate(
-                  min(10, state.trending.length),
-                  (index) {
-                    bool isSelected = currentIndex == index;
-                    return GestureDetector(
-                      onTap: () {
-                        scrollController.animateToPage(index);
-                      },
-                      child: AnimatedContainer(
-                        duration: Durations.medium1,
-                        height: 10,
-                        width: isSelected ? 25 : 10,
-                        margin: EdgeInsets.symmetric(
-                            horizontal: isSelected ? 6 : 3),
-                        decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(isSelected ? 20 : 10),
-                          color: isSelected
-                              ? Colors.white
-                              : Colors.grey.withOpacity(0.2),
-                        ),
-                      ),
-                    );
-                  },
-                )),
-              );
-            }),
-          ],
-        );
-      }
-      return Container();
-    });
   }
 }
