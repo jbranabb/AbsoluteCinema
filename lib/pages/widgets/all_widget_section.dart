31147e4fd1c7574393bb7f4aa4e5db83ecb1231e
jbranabb <zibranaby069@gmail.com>
2025-06-16 22:52:31 +0700

    feat(home & allWidgetSection) seprate the tabarview widget from home_page.dart and move it to another new file for more readable and clean code

---
+++ lib/pages/widgets/all_widget_section.dart
@@ -0,0 +1,234 @@
+
+import 'package:absolutecinema/state/bloc/home_bloc.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AllWidgetSection extends StatelessWidget {
+  const AllWidgetSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+  var height = MediaQuery.of(context).size.height;
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        if (state is StateLoaded){
+        return Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Trending This Week',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14),
+                  ),
+                  GestureDetector(
+                    child: Text(
+                      'see all',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: height * 0.209,
+              child: CarouselSlider.builder(
+                  itemCount: state.trending.length,
+                  itemBuilder:
+                      (context, index, realIndex) {
+                    var movies =
+                        state.trending[index];
+                    return Card(
+                      child: ClipRRect(
+                        borderRadius:
+                            BorderRadius.circular(
+                                12),
+                        child: Stack(
+                          children: [
+                            CachedNetworkImage(
+                                imageUrl:
+                                    'https://image.tmdb.org/t/p/w300${movies.posterPath}',
+                                placeholder:
+                                    (context, st) =>
+                                        Center(
+                                            child:
+                                                Text(
+                                          movies
+                                              .title,
+                                              textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              color:
+                                                  Colors.white60),
+                                        )),
+                                fit: BoxFit.cover),
+                            Positioned(
+                              right: 10,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons
+                                        .star_rounded,
+                                    color: Colors
+                                        .amber,
+                                  ),
+                                  Text(
+                                    movies.rate
+                                        .substring(
+                                            0, 3),
+                                    style: TextStyle(
+                                        color: Colors
+                                            .white),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  options: CarouselOptions(
+                      aspectRatio: 16 / 9,
+                      initialPage: 0,
+                      viewportFraction: 0.3)),
+            ),
+             Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Streaming Today',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14),
+                  ),
+                  GestureDetector(
+                    child: Text(
+                      'see all',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: height * 0.209,
+              child: CarouselSlider.builder(itemCount: state.streaming.length, 
+              itemBuilder:(context, index, realIndex) {
+                var tvshows = state.streaming[index];
+                return Card(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Stack(
+                      children: [
+                        CachedNetworkImage(
+                          fit: BoxFit.cover,
+                          imageUrl:'https://image.tmdb.org/t/p/w300${tvshows.posterPath}',
+                        placeholder: (context, url) =>  Center(child: Text(tvshows.title, textAlign: TextAlign.center,),),
+                        ),
+                        Positioned(
+                          right: 10,
+                          child: Row(
+                            children: [
+                              Icon(Icons.star_rounded, color: Colors.amber,),
+                              Text(tvshows.voteAvg.substring(0, 3), style: TextStyle(color: Colors.white),),
+                            ],
+                          ))
+                      ],
+                    ),
+                  ),
+                );
+              }, options: CarouselOptions(
+                aspectRatio: 16 / 9,
+                initialPage: 0,
+                viewportFraction: 0.3,
+                reverse: true
+              )),
+            ),
+             Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'In Theaters',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14),
+                  ),
+                  GestureDetector(
+                    child: Text(
+                      'see all',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: height * 0.21,
+              child: CarouselSlider.builder(itemCount: state.inTheaters.length, 
+              itemBuilder:(context, index, realIndex) {
+                var tvshows = state.inTheaters[index];
+                return Card(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Stack(
+                      children: [
+                        CachedNetworkImage(
+                          fit: BoxFit.cover,
+                          imageUrl:'https://image.tmdb.org/t/p/w300${tvshows.posterPath}',
+                        placeholder: (context, url) =>  Center(child: Text(tvshows.title, textAlign: TextAlign.center,),),
+                        ),
+                        Positioned(
+                          right: 10,
+                          child: Row(
+                            children: [
+                              Icon(Icons.star_rounded, color: Colors.amber,),
+                              Text(tvshows.voteAvg.substring(0, 3), style: TextStyle(color: Colors.white),),
+                            ],
+                          ))
+                      ],
+                    ),
+                  ),
+                );
+              }, options: CarouselOptions(
+                aspectRatio: 16 / 9,
+                initialPage: 0,
+                viewportFraction: 0.3,
+              )),
+            )
+            
+          ],
+        );
+        }else if(state is StateLoading){
+          return Center(
+            child: CircularProgressIndicator() ,
+          );
+        }
+        return Container();
+      }
+    );
+  }
+}
