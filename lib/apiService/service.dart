e3fefb7ecc5320f22bd4439ee35471542b036434
jbranabb <zibranaby069@gmail.com>
2025-06-24 21:47:42 +0700

    feat(service)addin new url for the api endpoint

--- lib/apiService/service.dart
+++ lib/apiService/service.dart
@@ -1,7 +1,8 @@
 import 'package:absolutecinema/apiService/model.dart';
 import 'package:dio/dio.dart';
 String imdbKey = 'd846efa91adca89e264b6aa72e2a3907';
-String urlname = 'https://api.themoviedb.org/3/tv/popular?api_key=d846efa91adca89e264b6aa72e2a3907';
+String urlname = 'https://api.themoviedb.org/3/tv/popular?api_key=$imdbKey';
+String genreUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=$imdbKey';
 Dio dio = Dio();
 void fetchData()async{
 var response = await dio.get(urlname);
@@ -14,6 +15,7 @@ List<Testing> finaldata = data.map((e)=> Testing.fromJson(e)).toList();
 for(var i = 0 ; i < 10; i++){
 var datafinal = finaldata[i].genreIds.replaceFirst('[', ' ').replaceAll(']',' ');
 print(datafinal);
+
 }
   }catch(e){
   print('gagal: $e');
