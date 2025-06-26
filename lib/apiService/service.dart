75b8d1b411ba3e1f39eb43f3cac9cc400feedf3b
jbranabb <zibranaby069@gmail.com>
2025-06-26 21:29:13 +0700

    feat(service) doing for loop in service for genres

--- lib/apiService/service.dart
+++ lib/apiService/service.dart
@@ -1,8 +1,27 @@
 import 'package:dio/dio.dart';
 
+String imdbKey = 'd846efa91adca89e264b6aa72e2a3907';
+String genreUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=$imdbKey';
+String movieUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=$imdbKey';
 Dio dio = Dio();
-
+late Map<String, dynamic> genreMap;
 void fetchDataGenres()async{
- 
+    var responseGenre =  await dio.get(genreUrl);
+    if(responseGenre.statusCode == 200){
+        print('berhasil get data');
+    
+       List<dynamic> genreList =  responseGenre.data['genres'];
+        genreMap = {
+        for (var g in genreList) g['id'].toString() : g['name'],
+       }; 
+       print(genreMap);
+    }else{
+        throw Exception('Gagal Ambil Data Genre');
+    }
+
 }
-void main(){}
+
+
+void main(){
+    fetchDataGenres();
+}
