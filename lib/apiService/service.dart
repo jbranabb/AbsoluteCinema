82aedc05a874af1d9edfa9ae75bd442666dc3041
jbranabb <zibranaby069@gmail.com>
2025-06-29 09:18:27 +0700

    feat(service) remove fetchDataGenres

--- lib/apiService/service.dart
+++ lib/apiService/service.dart
@@ -7,27 +7,6 @@ String genreUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=$imdbKe
 String movieUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=$imdbKey';
 Dio dio = Dio();
 late Map<String, dynamic> genreMap;
-void fetchDataGenres()async{
-    var responseGenre =  await dio.get(genreUrl);
-    if(responseGenre.statusCode == 200){
-        print('berhasil get data');
-    
-       List<dynamic> genreList =  responseGenre.data['genres'];
-        genreMap = {
-        for (var g in genreList) g['id'].toString() : g['name'],
-       }; 
-      //  print(genreMap);  // berhasil munculin genreMap nya  
-    }else{
-        throw Exception('Gagal Ambil Data Genre');
-    }
-    String setGrenreNames(List<dynamic> genreList){
-        return genreList.map((e) => genreMap[e.toString()] ?? 'unknown',).join(', ');
-        
-    }
-    
-    // final texFia =  fetchGenres(mov['genre_ids']).then((value) => print('value : $value'),);
-// print('hasil : $texFia');
-}
 
 
  Future<String> fetchGenres(
@@ -58,8 +37,7 @@ List<GenreFromApi> finaldata = dataFinal.map((x) => GenreFromApi.fromJson(x)).to
 List<dynamic> test = finaldata[1].genreIds.replaceFirst('[', '').replaceAll(']', '').split(' ');
 List<dynamic> loopingGenres = [
 for(var i = 0 ; i < finaldata.length ; i++){
- finaldata[i].genreIds.replaceFirst('[', '').replaceAll(']', '')
-}
+ finaldata[i].genreIds.replaceFirst('[', '').replaceAll(']', '')}
 ]; 
 
   // var listtest = dataFinal[1]['genre_ids'];
@@ -72,7 +50,7 @@ for(var i = 0 ; i < finaldata.length ; i++){
     },).join(', ');
   }
   return names(
-    test
+    loopingGenres
     );
 }
 
