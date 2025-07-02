f429203ab028b6cf1f6f43e2331a9d7598b0742d
jbranabb <zibranaby069@gmail.com>
2025-07-02 16:16:10 +0700

    feat(service) adding extraData for 'Director'

--- lib/apiService/service.dart
+++ lib/apiService/service.dart
@@ -2,3 +2,24 @@
 import 'package:dio/dio.dart';
 String imdbKey = 'd846efa91adca89e264b6aa72e2a3907';
 Dio dio = Dio();
+// Future<Map<String,dynamic>> extraData(int id)async {
+//     final detail = await dio.get('https://api.themoviedb.org/3/movie/$id?api_key=$imdbKey');
+//   final credits = await dio.get('https://api.themoviedb.org/3/movie/$id/credits?api_key=$imdbKey');
+
+
+// List<dynamic> director = credits.data['cast'];
+// final crew =  director.firstWhere();
+//   return {};
+// }
+
+void test()async{
+   final credits = await dio.get('https://api.themoviedb.org/3/movie/238/credits?api_key=$imdbKey');
+List<dynamic> director = credits.data['crew'];
+final crew = director.firstWhere((x) =>  x['job'] == 'Director',orElse: () => 'Unknown',);
+print(crew['name']);
+
+}
+void main()async{
+  print('object');
+    test();
+}
