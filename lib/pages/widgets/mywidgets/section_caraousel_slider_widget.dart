69004b60eaafb5c2757559aee30cd8735488adfe
jbranabb <zibranaby069@gmail.com>
2025-07-05 01:00:00 +0700

    feat(allwidgetSection) improving clean code and replace the dirt code to readablecode and clean

---
+++ lib/pages/widgets/mywidgets/section_caraousel_slider_widget.dart
@@ -0,0 +1,68 @@
+
+import 'package:absolutecinema/apiService/model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class SectionCaraouselSliderWidget extends StatelessWidget {
+   SectionCaraouselSliderWidget({
+    super.key,
+    required this.list
+  });
+  List<ConvertedModels> list;
+
+  @override
+  Widget build(BuildContext context) {
+    var height =  MediaQuery.of(context).size.height;
+    return SizedBox(
+      width: double.infinity,
+      height: height * 0.209,
+      child: CarouselSlider.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index, realIndex) {
+            var tvshows = list[index];
+            return Card(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Stack(
+                  children: [
+                    CachedNetworkImage(
+                      fit: BoxFit.cover,
+                      imageUrl:
+                          'https://image.tmdb.org/t/p/w300${tvshows.posterPath}',
+                      placeholder: (context, url) => Center(
+                        child: Text(
+                          tvshows.title,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                        right: 10,
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.star_rounded,
+                              color: Colors.amber,
+                            ),
+                            Text(
+                              tvshows.voteAvg.substring(0, 3),
+                              style: const TextStyle(
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ))
+                  ],
+                ),
+              ),
+            );
+          },
+          options: CarouselOptions(
+              aspectRatio: 16 / 9,
+              initialPage: 0,
+              viewportFraction: 0.3,
+              reverse: true)),
+    );
+  }
+}
