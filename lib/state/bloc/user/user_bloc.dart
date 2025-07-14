f378aeeb227ae2b38c362f6882a188264a0251d1
dnaz <zibranaby069@gmail.com>
2025-07-15 03:36:22 +0700

    feat(UserBloc) Addding genreMap in USerCredentials

--- lib/state/bloc/user/user_bloc.dart
+++ lib/state/bloc/user/user_bloc.dart
@@ -11,51 +11,71 @@ part 'user_state.dart';
 class UserBloc extends Bloc<UserEvent, UserState> {
   UserBloc() : super(UserInitial()) {
     Dio dio = Dio();
-    on<GetSessionUser>((event, emit)async {
+    on<GetSessionUser>((event, emit) async {
       emit(UserLoading());
-    SharedPreferences _prefs = await SharedPreferences.getInstance();
+      SharedPreferences _prefs = await SharedPreferences.getInstance();
       // handle terima event dari authbloc
-        String accountUrl  =  'https://api.themoviedb.org/3/account?session_id=${event.sesionId}&api_key=$imdbKey';
-        final response = await dio.get(accountUrl);
-        try{
-        if(response.statusCode == 200){
-          print('Succes : True'); 
+      String accountUrl =
+          'https://api.themoviedb.org/3/account?session_id=${event.sesionId}&api_key=$imdbKey';
+      final response = await dio.get(accountUrl);
+      try {
+        if (response.statusCode == 200) {
+          print('Succes : True');
           print("response : ${response.data}");
           // save the reponse data to the sharedprefrences
-        _prefs.setString('sessionId', event.sesionId);
-        _prefs.setString('username', response.data['username'] ?? 'invalid username');
-        _prefs.setString('id', response.data['id'] ?? 'invalid id');
-        }else{
+          _prefs.setString('sessionId', event.sesionId);
+          _prefs.setString(
+              'username', response.data['username'] ?? 'invalid username');
+          _prefs.setString('id', response.data['id'] ?? 'invalid id');
+        } else {
           emit(UserFailed(e: response.statusCode.toString()));
         }
-        }catch(e){
-          emit(UserFailed(e: e.toString()));
-        }
-    });
-    on<UserData>((event, emit)async {
-      emit(UserLoading());      
-    var id = pref?.getString('id');
-    var username = pref?.getString('username');
-    emit(UserLoaded(username: username!));
-    });
-    on<UserCredentials>((event, emit) async{
-    var id = pref?.getString('id');
-    var sesionId = pref?.getString('sessionId');
-    var headers = '?session_id=$sesionId&api_key=$imdbKey';
-    if(pref == null ||  id == null || sesionId == null){
-      emit(UserFailed(e: 'Failed null'));
-    }
-    //get fav
-    String favUrl = 'https://api.themoviedb.org/3/account/$id/favorite/${event.mediaType}$headers';
-    //get watchlist
-    String watchlistUrl = 'https://api.themoviedb.org/3/account/$id/watchlist/${event.mediaType}$headers';
-
-    var responseFAv = await  dio.get(favUrl);
-    var  responseWatchUrl  = await dio.get(watchlistUrl);
-    if(responseFAv.statusCode == 200 && responseWatchUrl.statusCode == 200){
-      final datafav = responseFAv.data['results'];
-      final dataWatchlist = responseWatchUrl.data['results'];
+      } catch (e) {
+        emit(UserFailed(e: e.toString()));
       }
+    });
+    on<UserData>((event, emit) async {
+      emit(UserLoading());
+      var id = pref?.getString('id');
+      var username = pref?.getString('username');
+      emit(UserLoaded(username: username!));
+    });
+    on<UserCredentials>((event, emit) async {
+      var id = pref?.getString('id');
+      var sesionId = pref?.getString('sessionId');
+      var headers = '?session_id=$sesionId&api_key=$imdbKey';
+      if (pref == null || id == null || sesionId == null) {
+        emit(UserFailed(e: 'Failed null'));
+      }
+      //get fav
+      String favUrl =
+          'https://api.themoviedb.org/3/account/$id/favorite/${event.mediaType}$headers';
+      //get watchlist
+      String watchlistUrl =
+          'https://api.themoviedb.org/3/account/$id/watchlist/${event.mediaType}$headers';
+
+      String genreUrlMov =
+          'https://api.themoviedb.org/3/genre/movie/list?api_key=$imdbKey';
+      String genreUrlTv =
+          'https://api.themoviedb.org/3/genre/tv/list?api_key=$imdbKey';
+
+      var responseFAv = await dio.get(favUrl);
+      var responseWatchUrl = await dio.get(watchlistUrl);
+      var responseGenreTv = await dio.get(genreUrlTv);
+      var responseGenreMov = await dio.get(genreUrlMov);
+    
+      if (responseFAv.statusCode == 200 && responseWatchUrl.statusCode == 200) {
+        final datafav = responseFAv.data['results'];
+        final dataWatchlist = responseWatchUrl.data['results'];
+      }
+    List dataGenreMov = responseGenreMov.data['genres'];
+    List dataGenreTv = responseGenreTv.data['genres'];
+    Map<String, dynamic> genreMapCombaine = {};
+
+    genreMapCombaine = {
+      for(var g in dataGenreTv) g['id'].toString() : g['name'],
+      for (var x in dataGenreMov) x['id'].toString() : x['name']
+    };
     });
   }
 }
