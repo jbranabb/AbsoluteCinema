4d188da03b28983f6d0547cf830e618a7bb29c87
dnaz <zibranaby069@gmail.com>
2025-07-16 04:36:57 +0700

    feat(Watchlist) adding popmenu button for changching the current mediatype between tvshows and tv

--- lib/pages/screens/watchlis.dart
+++ lib/pages/screens/watchlis.dart
@@ -6,7 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WatchlistPage extends StatefulWidget {
-   WatchlistPage({super.key,});
+  const WatchlistPage({
+    super.key,
+  });
 
   @override
   State<WatchlistPage> createState() => _WatchlistPageState();
@@ -17,60 +19,100 @@ class _WatchlistPageState extends State<WatchlistPage> {
   void initState() {
     super.initState();
     context.read<UserBloc>().add(UserCredentials(mediaType: 'movies'));
-   }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title: MyText(text: 'Watchlist', fnweight: FontWeight.bold, fnSize: 18,),
+      appBar: AppBar(
+        title: MyText(
+          text: 'Watchlist',
+          fnweight: FontWeight.bold,
+          fnSize: 18,
+        ),
         centerTitle: true,
-      ) ,
-      body: BlocBuilder<UserBloc, UserState>(builder: (context, state) {
-        if(state is UserDataLoaded){
-          if(state.dataWatchlist.isEmpty){
-            return Center(child: Text('Data kosong bro'),);
+        actions: [
+          PopupMenuButton(
+            onSelected: (value) {
+              if (value == 2) {
+                context.read<UserBloc>().add(UserCredentials(mediaType: 'tv'));
+              } else {
+                context
+                    .read<UserBloc>()
+                    .add(UserCredentials(mediaType: 'movies'));
+              }
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                child: Text('Movies'),
+                value: 1,
+              ),
+              const PopupMenuItem(
+                child: Text('Tv Show'),
+                value: 2,
+              ),
+            ],
+          )
+        ],
+      ),
+      body: BlocBuilder<UserBloc, UserState>(
+        builder: (context, state) {
+          if (state is UserDataLoaded) {
+            if (state.dataWatchlist.isEmpty) {
+              return const Center(
+                child: Text('Empty Bucket bro..\nadding something here'),
+              );
+            }
+            return GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4, childAspectRatio: 3 / 4),
+                itemCount: state.dataWatchlist.length,
+                itemBuilder: (context, index) {
+                  var movies = state.dataWatchlist[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(3.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(5),
+                          border: BoxBorder.all(
+                              color: Colors.blueGrey,
+                              style: BorderStyle.solid,
+                              width: 1.0)),
+                      child: ClipRRect(
+                          borderRadius: BorderRadiusGeometry.circular(5),
+                          child: CachedNetworkImage(
+                            imageUrl:
+                                'https://image.tmdb.org/t/p/w780${movies.posterPath}',
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) => Center(
+                              child: MyText(
+                                text: movies.title,
+                                fnweight: FontWeight.bold,
+                                fnSize: 11,
+                              ),
+                            ),
+                            errorWidget: (context, url, error) => const Center(
+                              child: Icon(Icons.help),
+                            ),
+                          )),
+                    ),
+                  );
+                });
+          } else if (state is UserLoading) {
+            return const Center(
+              child: LoadingWidget(),
+            );
+          } else if (state is UserFailed) {
+            return Center(
+              child: MyText(text: state.e),
+            );
           }
-          return GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4
-          ,childAspectRatio: 3 / 4),
-           itemCount: state.dataWatchlist.length,
-           itemBuilder:(context, index) {
-            var movies = state.dataWatchlist[index];
-             return Padding(
-               padding: const EdgeInsets.all(3.0),
-               child: Container(
-                decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(5),
-                border: BoxBorder.all(
-                  color: Colors.blueGrey,
-                  style: BorderStyle.solid,
-                  width: 1.0
-                )
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadiusGeometry.circular(5),
-                  child: CachedNetworkImage(imageUrl: 'https://image.tmdb.org/t/p/w780${movies.posterPath}', fit: BoxFit.cover,
-                  placeholder: (context, url) => Center(child: MyText(text: movies.title, fnweight: FontWeight.bold,
-                  fnSize: 11,
-                  ),),
-                  errorWidget: (context, url, error) => const Center(child: Icon(Icons.help),),
-                  )),
-               ),
-             );
-           });
-        }else  if(state is UserLoading){
-          return Center(
-            child: LoadingWidget(),
+          return Container(
+            color: Colors.red,
           );
-        }else if(state is UserFailed){
-          return Center(
-            child: MyText(text: state.e),
-          );
-        }
-        return Container(
-          color: Colors.red,
-        );
-      },),
+        },
+      ),
     );
   }
 }
