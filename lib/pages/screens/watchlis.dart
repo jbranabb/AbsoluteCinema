37a5938239a202e9759bb507db3a8319a75a3cb4
dnaz <zibranaby069@gmail.com>
2025-07-16 03:54:01 +0700

    feat(watchlist) adding cachedImage Network, and set the path to be an posterpath from state

--- lib/pages/screens/watchlis.dart
+++ lib/pages/screens/watchlis.dart
@@ -1,11 +1,12 @@
 import 'package:absolutecinema/pages/widgets/mywidgets/mytext.dart';
 import 'package:absolutecinema/pages/widgets/mywidgets/sectionWidget.dart';
 import 'package:absolutecinema/state/bloc/user/user_bloc.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WatchlistPage extends StatefulWidget {
-  const WatchlistPage({super.key});
+   WatchlistPage({super.key,});
 
   @override
   State<WatchlistPage> createState() => _WatchlistPageState();
@@ -29,13 +30,17 @@ class _WatchlistPageState extends State<WatchlistPage> {
           if(state.dataWatchlist.isEmpty){
             return Center(child: Text('Data kosong bro'),);
           }
-          return GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+          return GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4
+          ,childAspectRatio: 3 / 4),
            itemCount: state.dataWatchlist.length,
            itemBuilder:(context, index) {
-             return Container(
-              color: Colors.amber,
-              height: 10,
-              width: 20,
+            var movies = state.dataWatchlist[index];
+             return Padding(
+               padding: const EdgeInsets.all(2.0),
+               child: Container(
+                color: Colors.amber,
+                child: CachedNetworkImage(imageUrl: 'https://image.tmdb.org/t/p/w780${movies.posterPath}'),
+               ),
              );
            });
         }else  if(state is UserLoading){
