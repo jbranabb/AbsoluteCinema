29da83bf50a88fc84e85d2942a1ee0c3bb97ac07
dnaz <zibranaby069@gmail.com>
2025-07-17 03:27:27 +0700

    feat(setToggle) improving snackbar system

--- lib/state/cubit/set_toogle.dart
+++ lib/state/cubit/set_toogle.dart
@@ -12,23 +12,22 @@ class SetToogle extends Cubit<bool> {
     var headers = '?session_id=$sesionId&api_key=$imdbKey';
     String url = 'https://api.themoviedb.org/3/account/$id/watchlist$headers';
     // print(url);
+
     try{
     var responsePost =  await dio.post(url, data: {
       'media_type': mediaType,
       'media_id': mediaId,
       'watchlist': state
     });
-    if(responsePost.statusCode == 200){
+    if(responsePost.statusCode == 201 && mntd && state != false){
       print(responsePost.data);
+      ScaffoldMessenger.of(ctx).showSnackBar(const SnackBar(content: Text('Berhasil Menambahkan Ke Wacthlist')));
     print('Berhasil hore');
+    }else if(responsePost.statusCode == 200 && mntd && state !=true){
+      ScaffoldMessenger.of(ctx).showSnackBar(const SnackBar(content: Text('Menghapus Dari Wacthlist')));
     }
     print(responsePost.statusCode);
     print('Berhasil');
-    if(mntd && state != false){
-      ScaffoldMessenger.of(ctx).showSnackBar(const SnackBar(content: Text('Berhasil Menambahkan Ke Wacthlist')));
-    }else if(mntd && state !=true){
-      ScaffoldMessenger.of(ctx).showSnackBar(const SnackBar(content: Text('Menghapus Dari Wacthlist')));
-    }
     }catch(e){
     print(e);
     throw Exception(e);
