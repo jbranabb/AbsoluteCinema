afaf315b0d442e8e8bcbc4e654e05be0cd5799fb
dnaz <zibranaby069@gmail.com>
2025-07-18 01:32:00 +0700

    feat(profilePage) adding username and set mainaxissaligment to start

--- lib/pages/screens/profile.dart
+++ lib/pages/screens/profile.dart
@@ -6,42 +6,53 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       appBar: AppBar(
         title: MyText(text: 'Profile', fnSize: 18,fnweight: FontWeight.bold,),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-         SizedBox(   
-          height: 120,
-          child: Center(
-            child: Container(height: 80, width: 80, decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 41, 41, 40),
-              borderRadius: BorderRadius.circular(30),
-              border: BoxBorder.all(
-                width: 0.5,
-                color: Colors.blueGrey
-              ),
-              boxShadow:const [
-                BoxShadow(
-                  offset: Offset(5, 5),
-                  color: Color.fromARGB(80, 122, 122, 122),
-                  blurRadius: 10
-                ),
-              ] ,
-              
-              ),
-              
-             child: const Icon(Icons.person,size: 50,), ),
-          ),
-         ),
-    
-        
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+           SizedBox(   
+            height: 120,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(height: 80, width: 80, decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 41, 41, 40),
+                  borderRadius: BorderRadius.circular(30),
+                  border: BoxBorder.all(
+                    width: 0.5,
+                    color: Colors.grey.shade700
+                  ),
+                  boxShadow:const [
+                    BoxShadow(
+                      offset: Offset(5, 5),
+                      color: Color.fromARGB(80, 122, 122, 122),
+                      blurRadius: 10
+                    ),
+                  ] ,
+                  ),
+                  
+                 child: const Icon(Icons.person,size: 50,), ),
+                 MyText(text: 'Username', fnweight: FontWeight.bold, clors: Colors.grey.shade500, fnSize: 12,)
+              ],
+            ),
+           ),
+          Container(
+            color: Colors.red,
+        width:  width * 0.85,
+        height: 50,
+          )
+          
+          ],
+        ),
       ),
     );
   }
