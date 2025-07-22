b9a5a3244f1a505e560b692a00a995583b91a922
dnaz <zibranaby069@gmail.com>
2025-07-23 05:23:35 +0700

    feat(SearchPage) improving grid style

--- lib/pages/screens/search.dart
+++ lib/pages/screens/search.dart
@@ -47,63 +47,91 @@ class SearchPage extends StatelessWidget {
                     child: GridView.builder(
                       shrinkWrap: true,
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 1, mainAxisExtent: 200),
+                          crossAxisCount: 1, mainAxisExtent: 150),
                       itemCount: state.searching.length,
                       itemBuilder: (context, index) {
                         var mov = state.searching[index];
-                        print('object : ${mov.id}');
-                        print('object : ${mov.director}');
-                        print('object : ${mov.genreIds}');
-                        print('object : ${mov.relaseDate}');
-                        print('object : ${mov.title}');
-                        // print('object : ${mov.}');
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            // color: Colors.amber,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                children: [
-                                  Container(
-                                    height: 140,
-                                    width: 90,
+                        return Container(
+                          // color: Colors.amber,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 140,
+                                  width: 90,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      border: Border.all(
+                                          color: Colors.blueGrey, width: 0.5)),
+                                  child: ClipRRect(
+                                    borderRadius:
+                                        BorderRadiusGeometry.circular(10),
                                     child: CachedNetworkImage(
                                       imageUrl:
                                           'https://image.tmdb.org/t/p/w300${mov.posterPath}',
                                       fit: BoxFit.cover,
                                     ),
                                   ),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Container(
-                                    height: 120,
-                                    width: 200,
-                                    // color: Colors.red,
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        MyText(
-                                          text: mov.title,
-                                          fnweight: FontWeight.bold,
-                                        ),
-                                        Text(
-                                          mov.genreIds.join(', '),
-                                          maxLines: 2,
-                                          overflow: TextOverflow.fade,
-                                          style:const TextStyle(
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Container(
+                                  height: 120,
+                                  width: 200,
+                                  // color: Colors.red,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      MyText(
+                                        text: mov.title,
+                                        fnweight: FontWeight.bold,
+                                      ),
+                                      Text(
+                                        mov.genreIds.join(', '),
+                                        maxLines: 2,
+                                        overflow: TextOverflow.fade,
+                                        style: const TextStyle(
                                             color: Colors.grey,
-                                          fontWeight: FontWeight.w600),
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      Container(
+                                        height: 20,
+                                        width: 120,
+                                        // color: Colors.red,
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            MyText(
+                                              text: mov.relaseDate
+                                                  .substring(0, 4),
+                                              fnweight: FontWeight.w700,
+                                            ),
+                                            MyText(
+                                              text: '•',
+                                              fnweight: FontWeight.w800,
+                                              clors: Colors.grey.shade500,
+                                            ),
+                                            MyText(
+                                              text: 'DIRECTED BY',
+                                              clors: Colors.grey.shade600,
+                                              fnSize: 11,
+                                              fnweight: FontWeight.w600,
+                                            ),
+                                          ],
                                         ),
-                                        MyText(text: mov.relaseDate.substring(0,4), fnweight: FontWeight.w700,),
-                                        MyText(text: mov.director, fnweight: FontWeight.w500,)
-                                      ],
-                                    ),
+                                      ),
+                                      MyText(
+                                        text: mov.director,
+                                        fnweight: FontWeight.bold,
+                                      )
+                                    ],
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                           ),
                         );
