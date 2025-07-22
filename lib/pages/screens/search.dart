8cfc14f0c3508f9dff9af460ff2c3a48e9e39687
dnaz <zibranaby069@gmail.com>
2025-07-22 17:20:21 +0700

    feat(SearchPage) make sketch desgn of searching section

--- lib/pages/screens/search.dart
+++ lib/pages/screens/search.dart
@@ -1,5 +1,6 @@
 import 'package:absolutecinema/pages/widgets/mywidgets/mytext.dart';
 import 'package:absolutecinema/state/bloc/search/search_bloc.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,39 +15,74 @@ int page = 1;
         centerTitle: true, title: MyText(text: 'Search',  fnweight: FontWeight.bold,),
      
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              style: TextStyle(
-                color: Colors.white
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                controller: controllerText,
+                onSubmitted: (value) {
+                  print('value: $value');
+                  print('controller: $controllerText');
+                  context.read<SearchBloc>().add(Searching(querySeacrhing: value));
+                },
               ),
-              controller: controllerText,
-              onSubmitted: (value) {
-                print('value: $value');
-                print('controller: $controllerText');
-                context.read<SearchBloc>().add(Searching(querySeacrhing: value));
-              },
             ),
-          ),
-        BlocBuilder<SearchBloc, SearchState>(
-          builder: (context, state) {
-            if(state is SearchLoaded){return SizedBox(
-                  height: 580,
-                  child: ListView.builder(
-                    itemCount: state.searching.length,
-                    itemBuilder: (context, index) {
-                      var mov = state.searching[index];
-                      return ListTile(
-                      title: MyText(text:mov.title),
-                      );
-                  },),
-                );}
-                return Container();
-          },
-        )
-        ],
+          BlocBuilder<SearchBloc, SearchState>(
+            builder: (context, state) {
+              if(state is SearchInitial){
+                return Container(
+                  height: 200,
+                  color: Colors.red);
+              }
+              if(state is SearchLoaded){return SizedBox(
+                    height: 580,
+                    child: GridView.builder(
+                      shrinkWrap: true,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1, mainAxisExtent: 200),
+                      itemCount: state.searching.length,
+                      itemBuilder: (context, index) {
+                        var mov = state.searching[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            color: Colors.amber,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                Container(
+                                  height: 140,
+                                  width: 90,
+                                  decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(10)
+                                  ),
+                                  child: CachedNetworkImage(imageUrl: 'https://image.tmdb.org/t/p/w300${mov.posterPath}', fit: BoxFit.cover,),
+                                ),
+                                SizedBox(width: 10,),
+                                Container(
+                                  height: 120,
+                                  width: 200,
+                                  color: Colors.red,
+                                ),
+                              
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                    },),
+                  );}
+                  return Container();
+            },
+          )
+          ],
+        ),
       ),
     );
   }
